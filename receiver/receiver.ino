c32baa3ce7dedfe0b63f7df9125a0e3a77bfb963
necdnk <necdnk@73e20590-1326-bd1e-c9c6-0703921e748f>
2012-09-21 08:13:07 +0000

    arduino sketches. transmitt, receive command for homeeasy modules

---
+++ receiver/receiver.ino
@@ -0,0 +1,119 @@
+int rxPin = 12;
+ 
+ 
+void setup()
+{       pinMode(rxPin, INPUT);
+        Serial.begin(9600);
+}
+ 
+ 
+void loop()
+{
+        int i = 0;
+        unsigned long t = 0;
+ 
+        byte prevBit = 0;
+        byte bit = 0;
+ 
+        unsigned long sender = 0;
+        bool group = false;
+        bool on = false;
+        unsigned int recipient = 0;
+ 
+        // latch 1
+        while((t < 7480 || t > 12350))
+        {       t = pulseIn(rxPin, LOW, 1000000);
+        }
+ 
+        // latch 2
+        while(t < 2250 || t > 3000)
+        {       t = pulseIn(rxPin, LOW, 1000000);
+        }
+ 
+        // data
+        while(i < 64)
+        {
+                t = pulseIn(rxPin, LOW, 1000000);
+ 
+                if(t > 170 && t < 415)
+                {       bit = 0;
+                }
+                else if(t > 900 && t < 1460)
+                {       bit = 1;
+                }
+                else
+                {       i = 0;
+                        break;
+                }
+ 
+                if(i % 2 == 1)
+                {
+                        if((prevBit ^ bit) == 0)
+                        {       // must be either 01 or 10, cannot be 00 or 11
+                                i = 0;
+                                break;
+                        }
+ 
+                        if(i < 53)
+                        {       // first 26 data bits
+                                sender <<= 1;
+                                sender |= prevBit;
+                        }      
+                        else if(i == 53)
+                        {       // 26th data bit
+                                group = prevBit;
+                        }
+                        else if(i == 55)
+                        {       // 27th data bit
+                                on = prevBit;
+                        }
+                        else
+                        {       // last 4 data bits
+                                recipient <<= 1;
+                                recipient |= prevBit;
+                        }
+                }
+ 
+                prevBit = bit;
+                ++i;
+        }
+ 
+        // interpret message
+        if(i > 0)
+        {       printResult(sender, group, on, recipient);
+        }
+}
+ 
+ 
+void printResult(unsigned long sender, bool group, bool on, unsigned int recipient)
+{
+        Serial.print("<sender");
+        Serial.print(sender);
+        Serial.print("|");
+ 
+        if(group)
+        {       Serial.print("groupcommand");
+        }
+        else
+        {       Serial.print("groupno");
+        }
+        Serial.print("|");
+        
+        Serial.print("recipient");
+        Serial.print(recipient);
+        
+        Serial.print("|");
+        if(on)
+        {       Serial.print("stateon");
+        }
+        else
+        {       Serial.print("stateoff");
+        }
+        Serial.print("|");
+        
+        Serial.print(">"); 
+}
+
+
+
+
---
+++ transmitter/transmitter.ino
@@ -0,0 +1,176 @@
+bool bit2[26]={
+};              // 26 bit global to store the HE device address.
+int txPin = 2;                 // 433mhz transmitter on pin2
+
+String command; // String input from command prompt
+char inByte; // Byte input from command prompt
+char carray[10]; // character array for string to long // manipulation
+unsigned long sender=0;
+boolean group = false;
+boolean state = false;
+int recipient = 0;
+void setup()
+{
+
+  pinMode(txPin, OUTPUT);      // transmitter pin.
+  Serial.begin(9600);         // console port
+
+  Serial.println("go");
+  //sender5083278|groupno|stateoff|recipient10
+
+}
+
+void loop()
+{
+  // Input serial information:
+  if (Serial.available() > 0){
+    inByte = Serial.read();
+    // only input if a letter, number, =,?,+ are typed!
+    if ((inByte >= 65 && inByte <= 90) 
+      || (inByte >=97 && inByte <=122) 
+      || (inByte >= 48 && inByte <=57) 
+      || inByte == 43 || inByte == 61 || inByte == 63) {
+      command.concat(inByte);
+    }
+  }
+
+  if(inByte == '|')
+  {
+    if (command.startsWith("sender"))
+    {
+      command = command.substring(6);
+      command.toCharArray(carray, 10);
+      sender = atol(carray);
+      itob(sender, 26);            // convert our device code..
+    }
+    if (command.startsWith("recipient"))
+    {
+      command = command.substring(9);
+      command.toCharArray(carray, 10);
+      recipient = atol(carray);            // convert our device code..
+    }
+    if (command.startsWith("group"))
+    {
+      command = command.substring(5);
+      group = (command =="command");
+    }
+    if (command.startsWith("state")){
+      command = command.substring(5);
+        transmit(command == "on");            // send ON
+        delay(10);                 // wait (socket ignores us it appears unless we do this)
+        transmit(command == "on");            // send ON again
+        Serial.println("--------------");
+        
+        Serial.print("sender ");
+        Serial.println(sender, DEC);
+        
+        Serial.print("group ");
+        Serial.println(group, HEX);
+        
+        Serial.print("recipient ");
+        Serial.println(recipient, DEC);
+        
+        Serial.print("state ");
+        Serial.println(command == "on", HEX);
+        
+        Serial.println("--------------");
+    }
+    inByte=0;
+    command = "";
+  }
+}
+
+void transmit(int blnOn)
+{
+  int i;
+
+  // Do the latch sequence..
+  digitalWrite(txPin, HIGH);
+  delayMicroseconds(275);     // bit of radio shouting before we start.
+  digitalWrite(txPin, LOW);
+  delayMicroseconds(9900);     // low for 9900 for latch 1
+  digitalWrite(txPin, HIGH);   // high again
+  delayMicroseconds(275);      // wait a moment 275
+  digitalWrite(txPin, LOW);    // low again for 2675 - latch 2.
+  delayMicroseconds(2675);
+  // End on a high
+  digitalWrite(txPin, HIGH);
+
+  // Send HE Device Address..
+  // e.g. 1000010101000110010  272946 in binary.
+  for(i=0; i<26;i++)
+  {
+    sendPair(bit2[i]);
+  }
+
+  // Send 26th bit - group 1/0
+  sendPair(false);
+
+  // Send 27th bit - on/off 1/0
+  sendPair(blnOn);
+
+  // last 4 bits - recipient   -- button 1 on the HE300 set to
+  // slider position I in this example:
+
+  sendPair(recipient & 8);
+  sendPair(recipient & 4);
+  sendPair(recipient & 2);
+  sendPair(recipient & 1);
+
+  digitalWrite(txPin, HIGH);   // high again (shut up)
+  delayMicroseconds(275);      // wait a moment
+  digitalWrite(txPin, LOW);    // low again for 2675 - latch 2.
+
+}
+
+
+void sendBit(boolean b) {
+  if (b) {
+    digitalWrite(txPin, HIGH);
+    delayMicroseconds(310);   //275 orinally, but tweaked.
+    digitalWrite(txPin, LOW);
+    delayMicroseconds(1340);  //1225 orinally, but tweaked.
+  }
+  else {
+    digitalWrite(txPin, HIGH);
+    delayMicroseconds(310);   //275 orinally, but tweaked.
+    digitalWrite(txPin, LOW);
+    delayMicroseconds(310);   //275 orinally, but tweaked.
+  }
+}
+
+void sendPair(boolean b) {
+  // Send the Manchester Encoded data 01 or 10, never 11 or 00
+  if(b)
+  {
+    sendBit(true);
+    sendBit(false);
+  }
+  else
+  {
+    sendBit(false);
+    sendBit(true);
+  }
+}
+
+
+void itob(unsigned long integer, int length)
+{  //needs bit2[length]
+  // Convert long device code into binary (stores in global bit2 array.)
+  for (int i=0; i<length; i++){
+    if ((integer / power2(length-1-i))==1){
+      integer-=power2(length-1-i);
+      bit2[i]=1;
+    }
+    else bit2[i]=0;
+  }
+}
+
+unsigned long power2(int power){    //gives 2 to the (power)
+  unsigned long integer=1;          
+  for (int i=0; i<power; i++){      
+    integer*=2;
+  }
+  return integer;
+}
+
